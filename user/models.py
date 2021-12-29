82de82f1ae9ad24b0d788d720d0e4a4ec3a4b72d
mongus <kjyanus@gmail.com>
2021-12-29 16:50:57 +0900

    :sparkles: feat: Add User Model & Manager

--- user/models.py
+++ user/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from .managers import UserManager
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
+
+
+class User(AbstractUser):
+    email = models.EmailField(
+        _('email address'),
+        unique=True,
+        blank=False,
+        null=False,
+        error_messages={
+            'unique': _('A user with that email already exists.'),
+        }
+    )
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ["username"]
