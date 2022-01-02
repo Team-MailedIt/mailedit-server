8b7c1ae94ed2f8c3084a9917a152a64c628358ea
mongus <61130415+kynzun@users.noreply.github.com>
2022-01-02 23:29:40 +0900

    Feature user (#4)
    * :sparkles: feat: Add User Model & Manager
    
    * :sparkles: feat: Change Auth User Model to Custom User
    
    * :sparkles: feat: Add User Serializer & APIView
    
    * :sparkles: feat: signup url mapping
    
    * :sparkles: feat: Add user migration file
    
    * :white_check_mark: test: google login test
    
    * :sparkles: feat: Add settings for social auth
    
    * :label: chore: register User model on admin page & migrate
    
    * :sparkles: feat: Add Google Login View & URL Mapping
    
    * :rocket: test: Google Login Test Deploy
    
    * :label: chore: for deploy test
    
    * :label: chore: for deploy test
    
    * :bug: fix: downgrading django version to 3.0.8
    
    * :label: chore: for deploy test
    
    * :bug: test: deploy test
    
    * :bug: fix: change django version to 3.1
    
    * :sparkles: feat: delete migration files

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from .views import *
+from rest_framework_simplejwt.views import TokenRefreshView
+
 urlpatterns = [
     path("signup", SignUpAPIView.as_view()),
+    path("google/callback", GoogleLoginCallBackView.as_view()),
+    path('google/login', GoogleLoginView.as_view()),
+    path('token/refresh', TokenRefreshView.as_view()),
+    path('test', TestAuthView.as_view()),
 ]
