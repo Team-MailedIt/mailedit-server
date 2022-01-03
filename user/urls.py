9598529489df125274c165892dcd158a33d3846d
mongus <kjyanus@gmail.com>
2022-01-03 19:51:56 +0900

    :sparkles: feat: edit view & url

--- user/urls.py
+++ user/urls.py
@@ -4,8 +4,9 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("signup", EmailRegisterAPIView.as_view()),
+    path("google/redirect", GoogleRedirectView.as_view()),
     path("google/callback", GoogleLoginCallBackView.as_view()),
-    path('google/login', GoogleLoginView.as_view()),
-    path('token/refresh', TokenRefreshView.as_view()),
-    path('test', TestAuthView.as_view()),
+    path("google/login", GoogleLoginView.as_view()),
+    path("token/refresh", TokenRefreshView.as_view()),
+    path("test", TestAuthView.as_view()),
 ]
