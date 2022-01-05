98a2a729f452c9ccaa8c3ee192acc1c3a5924011
hywn <14hhan@gmail.com>
2022-01-05 17:16:31 +0900

    🚨 fix: 누락된 URL 등록

--- user/urls.py
+++ user/urls.py
@@ -4,6 +4,8 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("signup", EmailRegisterAPIView.as_view()),
+    path("signin", EmailLoginAPIView.as_view()),
+    path("retry_verification", RetryVerificationAPI.as_view()),
     path("google/login", GoogleLoginAPIView.as_view()),
     path("token/refresh", TokenRefreshView.as_view()),
     path("test", TestAuthView.as_view()),
