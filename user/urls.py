7954aa966a90f5cc1e1a7fa97f285356b2451bac
mongus <kjyanus@gmail.com>
2022-01-19 19:59:21 +0900

    :sparkles: feat: Add User Checking API

--- user/urls.py
+++ user/urls.py
@@ -5,8 +5,8 @@ from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path("signup", EmailRegisterAPIView.as_view()),
     path("login", EmailLoginAPIView.as_view()),
+    path("user-check", UserCheckView.as_view()),
     path("retry-verification", RetryVerificationAPI.as_view()),
     path("login/google", GoogleLoginAPIView.as_view()),
     path("token/refresh", TokenRefreshView.as_view()),
-    path("test", TestAuthView.as_view()),
 ]
