91f8f4900c9e15f3a89770ba91789b24ccc30d89
mongus <kjyanus@gmail.com>
2022-01-24 17:07:49 +0900

    :ambulance: fix: BaseTemplate subtitle max_length to 50

--- service/models.py
+++ service/models.py
@@ -75,7 +75,7 @@ class BaseTemplate(models.Model):
     )
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=50, blank=False, null=False)
-    subtitle = models.CharField(max_length=30, blank=True, null=False)
+    subtitle = models.CharField(max_length=50, blank=True, null=False)
     category = models.CharField(
         max_length=6,
         null=False,
